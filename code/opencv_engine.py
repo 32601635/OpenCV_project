1961a222a40d05367cfe24b49f15b3724c0f828b
32601635 <onspeednpc999@gmail.com>
2022-06-07 17:59:44 +0800

    20220606_02

---
+++ code/opencv_engine.py
@@ -0,0 +1,19 @@
+import cv2
+
+class opencv_engine(object):
+
+    @staticmethod
+    def point_float_to_int(point):
+        return (int(point[0]), int(point[1]))
+
+    @staticmethod
+    def read_image(file_path):
+        return cv2.imread(file_path)
+
+    @staticmethod
+    def draw_point(img, point=(0, 0), color = (0, 0, 255)): # red
+        point = opencv_engine.point_float_to_int(point)
+        print(f"get {point=}")
+        point_size = 1
+        thickness = 4
+        return cv2.circle(img, point, point_size, color, thickness)
